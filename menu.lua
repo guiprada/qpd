3d5220eef132e37eac16d90a8448d55af03a2380
Guilherme Cunha Prada <guiprada@gmail.com>
2019-12-12 16:39:00 -0300

    new particle actor

--- menu.lua
+++ menu.lua
@@ -1,6 +1,7 @@
 local utils = require "utils"
 local gamestate = require "gamestate"
 local player = require "player"
+local particle = require "particle"
 
 local menu = {}
 
@@ -13,12 +14,26 @@ function menu.load(this_player)
     menu.width = love.graphics.getWidth()
     menu.height = love.graphics.getHeight()
 
-    print(this_player)
     menu.player = this_player or player.new()
+
+    menu.particles = {}
+    for i=1,20,1 do
+        menu.particles[i] = particle.new()
+    end
 end
 
 function menu.draw()
+    love.graphics.setColor(1,1,1)
     love.graphics.printf(menu.text, 0, 3*menu.height/4, menu.width, "center")
+    for i=1,20,1 do
+        menu.particles[i]:draw()
+    end
+end
+
+function menu.update(dt)
+    for i=1,20,1 do
+        menu.particles[i]:update(dt)
+    end
 end
 
 function menu.keypressed(key, scancode, isrepeat)
