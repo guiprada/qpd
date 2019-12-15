f978ee1923c886166286e735f520c9d9fcb1adfe
Guilherme Cunha Prada <guiprada@gmail.com>
2019-12-15 19:52:34 -0300

    finished camera

--- game/camera.lua
+++ game/camera.lua
@@ -3,7 +3,9 @@ local camera = {}
 function camera.new()
     local o = {}
     o.x = 0
+    o.y = 0
     o.speed_x = 0
+    o.speed_y = 0
 
     o.set = camera.set
     o.update = camera.update
@@ -12,11 +14,12 @@ function camera.new()
 end
 
 function camera:set()
-    love.graphics.translate(self.x, 0)
+    love.graphics.translate(self.x, self.y)
 end
 
 function camera:update(dt)
     self.x = self.x + self.speed_x*dt
+    self.y = self.y + self.speed_y*dt
 end
 
 return camera
--- game/conf.lua
+++ game/conf.lua
@@ -1,10 +1,10 @@
 -- Configuration
 function love.conf(t)
 	t.title = "o melhor jogo"
-	t.window.width = 1024
-	t.window.height = 768
+	t.window.width = 1920
+	t.window.height = 1080
 	t.console = true
-
+	t.window.fullscreen = true
   --t.window.borderless = false
   --t.window.resizable = true
 	t.window.vsync = 1
